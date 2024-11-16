b641f259cb921aa7412b30ba00962ef82967b5d0
Jacob Heric <jacob@jacobheric.com>
2024-11-16 11:45:59 -0500

    pass along infer body as raw option

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,17 +1,56 @@
 import { generate } from "../ai/generate.ts";
+import { OPENAI_API_KEY } from "@/lib/config.ts";
+
 import { inngest } from "./client.ts";
+import { openai } from "inngest";
 
 export const generateText = inngest.createFunction(
   { id: "generate-text" },
   { event: "ai/generate.text" },
   async ({ event, step }) => {
     const { provider, model, prompt } = event.data;
-    const response: any = await step.ai(
+    const response: any = await step.ai.wrap(
       "generateText",
       generate,
       { prompt, provider, model },
     );
 
     return { event, body: response };
+  },
+);
+
+export const aiInfer = inngest.createFunction(
+  { id: "ai-infer-test" },
+  { event: "ai/infer.test" },
+  async ({ event, step }) => {
+    console.log("running ai infer");
+    console.log("OPENAI_API_KEY", OPENAI_API_KEY);
+    const response = await step.ai.infer("inference", {
+      opts: openai(OPENAI_API_KEY),
+      body: {
+        model: "chatgpt-4o-latest",
+        messages: [{
+          role: "user",
+          content: "Give me a famous quote about sidewalks and rain.",
+        }],
+        tools: [],
+      },
+    });
+
+    console.log("response", response);
+    return { event, body: response };
+  },
+);
+
+export const testConcurrency = inngest.createFunction(
+  {
+    id: "test-concurrency",
+    concurrency: { limit: 1, key: "test.concurrency" },
+  },
+  { event: "test/concurrency" },
+  async ({ event, step }) => {
+    console.log("sleeping for 30s");
+    await step.sleep("wait-a-moment", "30s");
+    return { event, body: "I'm awake!" };
   },
 );
--- routes/api/inngest.ts
+++ routes/api/inngest.ts
@@ -1,8 +1,12 @@
 import { serve } from "inngest/deno/fresh";
 import { inngest } from "@/lib/inngest/client.ts";
-import { generateText } from "@/lib/inngest/functions.ts";
+import {
+  aiInfer,
+  generateText,
+  testConcurrency,
+} from "@/lib/inngest/functions.ts";
 
 export const handler = serve({
   client: inngest,
-  functions: [generateText],
+  functions: [generateText, aiInfer, testConcurrency],
 });
