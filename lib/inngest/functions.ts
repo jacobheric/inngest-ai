7e161cf311ad4ebf9d466d8f96c912e75ebcdf20
Jacob Heric <jacob@jacobheric.com>
2024-11-25 09:51:35 -0500

    use latest

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -9,7 +9,8 @@ export const generateText = inngest.createFunction(
   { event: "ai/generate.text" },
   async ({ event, step }) => {
     const { provider, model, prompt } = event.data;
-    const response: any = await step.ai.wrap(
+    console.log("running ai wrap");
+    const response = await step.ai.wrap(
       "generateText",
       generate,
       { prompt, provider, model },
@@ -24,20 +25,21 @@ export const aiInfer = inngest.createFunction(
   { event: "ai/infer.test" },
   async ({ event, step }) => {
     console.log("running ai infer");
-    console.log("OPENAI_API_KEY", OPENAI_API_KEY);
     const response = await step.ai.infer("inference", {
-      opts: openai(OPENAI_API_KEY),
-      body: {
+      model: openai({
+        apiKey: OPENAI_API_KEY,
         model: "chatgpt-4o-latest",
+      }),
+      body: {
+        temperature: .9,
         messages: [{
           role: "user",
           content: "Give me a famous quote about sidewalks and rain.",
         }],
-        tools: [],
       },
     });
 
-    console.log("response", response);
+    console.log("ai infer esponse", response);
     return { event, body: response };
   },
 );
